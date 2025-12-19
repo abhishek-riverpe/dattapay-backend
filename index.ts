59fa492758491cbacce1da4df9258c0d06ff2cb4
= <=>
2025-12-19 11:37:57 +0600

    add express-rate-limit middleware for prevent brute-force & DoS attacks

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import rateLimit from "express-rate-limit";
 import helmet from "helmet";
 import logger from "./lib/logger";
 
@@ -8,8 +9,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
 app.use(helmet());
+app.use(cors());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
