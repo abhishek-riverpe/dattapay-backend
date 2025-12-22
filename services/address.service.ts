52139a19daf5021b489f990645e4fabee41c36b0
= <=>
2025-12-23 00:52:13 +0600

    Improve routes

--- services/address.service.ts
+++ services/address.service.ts
@@ -37,8 +37,6 @@ class AddressService {
     if (existingAddress) {
       throw new Error(409, "User already has an address");
     }
-    await userRepository.update(user.id, { accountStatus: "PENDING" });
-
     return addressRepository.create(data);
   }
 
