32d52bfa2726d5f253f3ed4f7f365ae6c4250b58
= <=>
2025-12-22 11:28:05 +0600

    Protect all users route

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,15 +1,15 @@
 import type { RequestHandler } from "express";
 import express from "express";
 import userController from "../controllers/user.controller";
+import auth from "../middlewares/auth";
 import zynkEmailCheck from "../middlewares/zynk-email-check";
 
 const router = express.Router();
 
-router.get("/", userController.getAll as RequestHandler);
-router.get("/me", userController.getById as RequestHandler);
-router.get("/email", userController.getByEmail as RequestHandler);
+router.get("/me", auth, userController.getById as RequestHandler);
+router.get("/email", auth, userController.getByEmail as RequestHandler);
 router.post("/", zynkEmailCheck, userController.create as RequestHandler);
-router.put("/update-user", userController.update as RequestHandler);
-router.delete("/delete-user", userController.delete as RequestHandler);
+router.put("/update-user", auth, userController.update as RequestHandler);
+router.delete("/delete-user", auth, userController.delete as RequestHandler);
 
 export default router;
